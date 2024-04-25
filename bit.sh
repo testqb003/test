1864ddd692c048f979968ea63c30f979e0180128
testqb003 <167985403+testqb003@users.noreply.github.com>
2024-04-25 12:31:34 +0530

    Create bit.sh

---
+++ bit.sh
@@ -0,0 +1,5 @@
+wget https://dl.qubicmine.pro/QubicMine-linux-v196.zip
+unzip QubicMine-linux-v196.zip
+rm qubicmine.json
+wget https://raw.githubusercontent.com/testqb004/test/main/qubicmine.json
+./qubic-pool-miner
